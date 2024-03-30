12384a030829f9100099dcf45d640c28b0837ac7
Geoffrey Martin <geoffm39@gmail.com>
2024-03-30 18:15:03 +1100

    remove flask-gravatar

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_bootstrap import Bootstrap5
 from dotenv import load_dotenv
 from flask_ckeditor import CKEditor
-# from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
@@ -11,15 +10,6 @@ load_dotenv()
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap5(app)
 ckeditor = CKEditor(app)
-
-# gravatar = Gravatar(app,
-#                     size=100,
-#                     rating='g',
-#                     default='retro',
-#                     force_default=False,
-#                     force_lower=False,
-#                     use_ssl=False,
-#                     base_url=None)
 
 
 from app import routes
