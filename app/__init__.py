c2cfb53f7e37ba00bdeb951ae485cf8b52cd1f06
Geoffrey Martin <geoffm39@gmail.com>
2024-03-30 20:01:42 +1100

    add models.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_bootstrap import Bootstrap5
 from dotenv import load_dotenv
 from flask_ckeditor import CKEditor
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
@@ -11,5 +13,11 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap5(app)
 ckeditor = CKEditor(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+db = SQLAlchemy()
+db.init_app(app)
 
-from app import routes
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,17 @@
 from flask import render_template, redirect, url_for, flash, abort
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
+from libgravatar import Gravatar
 from functools import wraps
 
-from app import app
+from app import app, login_manager, db
 from app.forms import LoginForm, RegisterForm, AddCafeForm, CommentForm
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404()
 
 
 @app.route('/')
