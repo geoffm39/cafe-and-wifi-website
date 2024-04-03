7407052f9df93b07e0c2ad9cba46ae57cf95f01e
Geoffrey Martin <geoffm39@gmail.com>
2024-04-04 08:57:49 +1100

    remove libgravatar

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,7 @@ from flask import render_template, redirect, url_for, flash, abort
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
-from libgravatar import Gravatar
 from functools import wraps
-
 from app import app, login_manager, db
 from app.models import User
 from app.forms import LoginForm, RegisterForm, AddCafeForm, CommentForm
