7d040353e5a26075897eb91a2b295864d7e52ba7
Geoffrey Martin <geoffm39@gmail.com>
2024-04-04 22:49:24 +1100

    rough in login code

--- app/routes.py
+++ app/routes.py
@@ -18,13 +18,29 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    return render_template('login.html')
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-    return render_template('register.html')
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.execute(db.Select(User).where(User.email == form.email.data)).scalar()
+        if not user:
+            flash('Wrong email. Try again')
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, form.password.data):
+            flash('Wrong password. Try again')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('home'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    redirect(url_for('home'))
