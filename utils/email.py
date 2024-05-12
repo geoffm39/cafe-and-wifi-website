f9dc5c3e620fa058ed42e848615e1a504d5d160d
Geoffrey Martin <geoffm39@gmail.com>
2024-05-12 18:16:58 +1000

    complete email.py

--- utils/email.py
+++ utils/email.py
@@ -6,10 +6,19 @@ ADMIN_EMAIL_PASSWORD = os.environ.get('ADMIN_EMAIL_PASSWORD')
 SMTP_SERVER = os.environ.get('SMTP_SERVER')
 
 
-def send_email(name, email, subject, message):
+def send_email(email, subject, message):
     with smtplib.SMTP(SMTP_SERVER, port=587) as connection:
         connection.starttls()
         connection.login(ADMIN_EMAIL, ADMIN_EMAIL_PASSWORD)
         connection.sendmail(from_addr=ADMIN_EMAIL,
                             to_addrs=email,
-                            msg=f"Subject:Portfolio Message\n\nName: {name}\nEmail: {email}\nSubject: {subject}\n{message}")
+                            msg=f"Subject:{subject}\n\n{message}")
+
+
+def receive_email(subject, message):
+    with smtplib.SMTP(SMTP_SERVER, port=587) as connection:
+        connection.starttls()
+        connection.login(ADMIN_EMAIL, ADMIN_EMAIL_PASSWORD)
+        connection.sendmail(from_addr=ADMIN_EMAIL,
+                            to_addrs=ADMIN_EMAIL,
+                            msg=f"Subject:{subject}\n\n{message}")
